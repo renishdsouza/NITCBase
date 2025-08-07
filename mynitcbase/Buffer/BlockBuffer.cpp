bb564b0bdac4a7ffe0a097fcf7077553c0128732
renish_dsouza <renish_b231198cs@nitc.ac.in>
2025-08-07 08:25:25 +0530

    blockbuffer.cpp with ex

--- mynitcbase/Buffer/BlockBuffer.cpp
+++ mynitcbase/Buffer/BlockBuffer.cpp
@@ -2,3 +2,56 @@
 
 #include <cstdlib>
 #include <cstring>
+// the declarations for these functions can be found in "BlockBuffer.h"
+
+BlockBuffer::BlockBuffer(int blockNum) {
+  // initialise this.blockNum with the argument
+  this->blockNum=blockNum;
+}
+
+// calls the parent class constructor
+RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum) {}
+
+// load the block header into the argument pointer
+int BlockBuffer::getHeader(struct HeadInfo *head) {
+  
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, this->blockNum);
+  // read the block at this.blockNum into the buffer
+  
+  // populate the numEntries, numAttrs and numSlots fields in *head
+  memcpy(&head->numSlots, buffer + 24, 4);
+  memcpy(&head->numEntries, /* fill this */buffer+16, 4);
+  memcpy(&head->numAttrs, /* fill this */buffer+20, 4);
+  memcpy(&head->rblock, /* fill this */buffer+12, 4);
+  memcpy(&head->lblock, /* fill this */buffer+8, 4);
+  memcpy(&head->pblock,buffer+4, 4);
+
+  return SUCCESS;
+}
+
+// load the record at slotNum into the argument pointer
+int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
+  struct HeadInfo head;
+  
+  // get the header using this.getHeader() function
+  BlockBuffer::getHeader(&head);
+  int attrCount = head.numAttrs;
+  int slotCount = head.numSlots;
+
+  // read the block at this.blockNum into a buffer
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer,this->blockNum);
+
+  /* record at slotNum will be at offset HEADER_SIZE + slotMapSize + (recordSize * slotNum)
+     - each record will have size attrCount * ATTR_SIZE
+     - slotMap will be of size slotCount
+  */
+  int recordSize = attrCount * ATTR_SIZE;
+  unsigned char *slotPointer = /* calculate buffer + offset */buffer+(32+slotCount+(recordSize+slotNum));
+
+  // load the record into the rec data structure
+  memcpy(rec, slotPointer, recordSize);
+
+  return SUCCESS;
+}
